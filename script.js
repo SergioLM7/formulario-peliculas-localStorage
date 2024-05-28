32448a8cff681f3a00a8f4a04e10908d3e00052d
SergioLM <sergiolillom@gmail.com>
2024-05-28 08:07:10 +0200

    Funcionalidad localStorage aplicada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 //Variables
 const formulario = document.querySelector("#formPeliculas");
 const selectUsuario = document.querySelector("#genero");
@@ -15,24 +17,27 @@ let cuerpoTabla = document.querySelector('#cuerpoTabla');
 let arrayPeliculaCorrecto = [];
 let errores = '';
 let fechaActual = new Date().getFullYear();
+let peliculasAImprimir = JSON.parse(localStorage.getItem("peliculas")) || [];
+
 
 //Eventos
-formulario.addEventListener(('submit'), (evento)=>{
-        evento.preventDefault();
-        validarForm();
-    });
+formulario.addEventListener(('submit'), (evento) => {
+    evento.preventDefault();
+    validarForm();
+    pintarDatos(peliculasAImprimir);
+});
 
 selectFiltro.addEventListener('change', (evento) => {
-        cuerpoTabla.innerHTML = '';
-        let optionValue = evento.target.value;
-        filtrarPorGenero(arrayPeliculaCorrecto, optionValue);        
+    cuerpoTabla.innerHTML = '';
+    let optionValue = evento.target.value;
+    filtrarPorGenero(peliculasAImprimir, optionValue);
 });
 
 
 //Funciones
 //Función para crear los options, tanto del selectUsuario, como del selectFiltro
 const crearGeneros = (...array) => {
-    array.forEach((elemento)=> {
+    array.forEach((elemento) => {
         let option = document.createElement('option');
         option.value = elemento;
         option.textContent = elemento;
@@ -51,6 +56,8 @@ const crearGeneros = (...array) => {
         select.append(fragment);
     });
 };*/
+
+
 
 //Función validadora del form
 const validarForm = () => {
@@ -76,7 +83,7 @@ const validarForm = () => {
             objetoCorrecto.director = objetoPelicula.director
         }
     };
-   
+
     if (objetoPelicula.año) {
         if (!regExpPeliculas.año.test(objetoPelicula.año) || objetoPelicula.año > fechaActual || objetoPelicula.año < 1800) {
             errores += '<li>Año incorrecto</li>';
@@ -85,24 +92,23 @@ const validarForm = () => {
         }
     };
 
-    
+
     if (objetoPelicula.genero === '--selecciona un género--') {
         errores += '<li>Título incorrecto</li>';
     } else {
         objetoCorrecto.genero = objetoPelicula.genero;
     }
 
-    if (errores==='') {
-        pintarDatos(objetoCorrecto);
-        arrayPeliculaCorrecto.push(objetoCorrecto);
-        selectFiltro.removeAttribute('disabled');
+    if (errores === '') {
+        peliculasAImprimir.push(objetoCorrecto);
+        localStorage.setItem("peliculas", JSON.stringify(peliculasAImprimir));
         objetoCorrecto={};
-        formulario.reset(); 
+        formulario.reset();
     } else {
         alert('Alguno de los campos no es válido.');
         errores = '';
     };
-    
+
 };
 
 //Función que vuelca los option en cada select
@@ -112,22 +118,31 @@ const pintarGeneros = () => {
 };
 
 //Función pintadora de los datos en la tabla
-const pintarDatos = (objeto) => {
-
-    const filaTabla = document.createElement('tr');
-    let correcto = Object.values(objeto);
-
-    correcto.forEach((elemento) => {
-       let celdaTabla = document.createElement('td');
-       celdaTabla.innerHTML = elemento;
-       fragment.append(celdaTabla);
+const pintarDatos = (array) => {
+    // const productosCarrito = JSON.parse(localStorage.getItem("peliculas"));
+    //console.log(productosCarrito);
+    cuerpoTabla.innerHTML = '';
+    array.forEach((objeto) => {
+        const filaTabla = document.createElement('tr');
+        let celdaTabla = document.createElement('td');
+        celdaTabla.innerHTML = objeto.titulo;
+        filaTabla.append(celdaTabla);
+        let celdaTabla2 = document.createElement('td');
+        celdaTabla2.innerHTML = objeto.director;
+        filaTabla.append(celdaTabla2);
+        let celdaTabla3 = document.createElement('td');
+        celdaTabla3.innerHTML = objeto.año;
+        filaTabla.append(celdaTabla3);
+        let celdaTabla4 = document.createElement('td');
+        celdaTabla4.innerHTML = objeto.genero;
+        filaTabla.append(celdaTabla4);
+        cuerpoTabla.append(filaTabla);
     });
-    filaTabla.append(fragment);
-    cuerpoTabla.append(filaTabla);
+    selectFiltro.removeAttribute('disabled');
 };
 
 //Función pintadora de datos en la tabla para Arrays
-const pintarDatosArray = (array) => {
+/*const pintarDatosArray = (array) => {
 
     array.forEach(({titulo, director, año, genero}) => {
         let filaTabla = document.createElement('tr');
@@ -138,41 +153,45 @@ const pintarDatosArray = (array) => {
         cuerpoTabla.append(filaTabla);
     });
   
-};
+};*/
 
 //Función para filtrar las películas según los option del select #filtro
 const filtrarPorGenero = (array, value) => {
     cuerpoTabla.innerHTML = '';
-    let arrayFiltrado = [];
+    let arrayFiltrado = JSON.parse(localStorage.getItem("peliculas")) || [];
+    let arrayNuevo = [];
 
-     if (value === 'Todos') {
-        arrayFiltrado = array;
-        arrayFiltrado.forEach((pelicula) => pintarDatos(pelicula));
-     } else {
-        arrayFiltrado = array.filter((elemento) => elemento.genero === value); 
-        arrayFiltrado.forEach((pelicula) => pintarDatos(pelicula));
-     };
+    if (value === 'Todos') {
+        arrayFiltrado.forEach((pelicula) => arrayNuevo.push(pelicula));
+        pintarDatos(arrayNuevo);
+    } else {
+        arrayNuevo = arrayFiltrado.filter((objeto) => objeto.genero === value);
+        pintarDatos(arrayNuevo);
+    };
 
-     if (arrayFiltrado.length === 0) {
+    if (arrayFiltrado.length === 0) {
         const generoVacio = document.createElement('td');
         generoVacio.textContent = 'Lo sentimos. No hay ninguna película que mostrar de este género.';
         cuerpoTabla.append(generoVacio);
-     };
+    };
 
 };
 //Invocación funciones
 pintarGeneros();
+pintarDatos(peliculasAImprimir);
 
 
-        /*(elemento)=> {
-        if (elemento.genero === 'Comedia') {
-            arrayPeliculasComedia.push(elemento);
-        } else if (elemento.genero === 'Terror') {
-            arrayPeliculasTerror.push(elemento);
-        } else if (elemento.genero === 'Accion') {
-            arrayPeliculasAccion.push(elemento);
-        } else {
-            arrayPeliculasRomantica.push(elemento);
-        }
-    });*/
 
+/*(elemento)=> {
+if (elemento.genero === 'Comedia') {
+    arrayPeliculasComedia.push(elemento);
+} else if (elemento.genero === 'Terror') {
+    arrayPeliculasTerror.push(elemento);
+} else if (elemento.genero === 'Accion') {
+    arrayPeliculasAccion.push(elemento);
+} else {
+    arrayPeliculasRomantica.push(elemento);
+}
+});*/
+
+}); //LOADED
